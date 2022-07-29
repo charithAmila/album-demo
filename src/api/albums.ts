5d059271e7948fc7f4df7b3584095988bf8b51f5
charithAmila <dkcadissanayaka@gmail.com>
2022-07-29 11:48:46 +0530

    Add comments for functions

--- src/api/albums.ts
+++ src/api/albums.ts
@@ -1,13 +1,23 @@
 const API_KEY = `d732731be2f5f0ec4b10e5a3607d7090`;
 const BASE_URL = `https://ws.audioscrobbler.com/2.0/`;
 
-export const getAlbums = () => {
+/**
+ * Function that get albums
+ * @returns {Promise}
+ */
+export const getAlbums = (): Promise<any> => {
   return fetch(
     `${BASE_URL}?method=tag.gettopalbums&tag=rock&api_key=${API_KEY}&format=json`
   );
 };
 
-export const getAlbumsInfo = (artist: string, album: string) => {
+/**
+ * Function that get album info
+ * @param artist {String} name    Name of the artist
+ * @param album {String} name   Name of the album
+ * @returns {Promise}
+ */
+export const getAlbumsInfo = (artist: string, album: string): Promise<any> => {
   return fetch(
     `${BASE_URL}?method=album.getinfo&api_key=${API_KEY}&artist=${artist}&album=${album}&format=json`
   );
--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,11 +1,27 @@
 import { AlbumInterface } from "../types/album";
 
+/**
+ * Function that find album by name
+ * @param albums {AlbumInterface[]}
+ * @param albumName {String}
+ * @returns {AlbumInterface | null}
+ */
 export const findAlbumByName = (
   albums: AlbumInterface[],
   albumName: string | undefined | null
-) => albums.find(({ name }) => name === albumName) || null;
+): AlbumInterface | null =>
+  albums.find(({ name }) => name === albumName) || null;
 
-export const searchAlbumByName = (albums: AlbumInterface[], text: string) => {
+/**
+ * Function that search album by name
+ * @param albums {AlbumInterface[]}
+ * @param text {String}
+ * @returns {AlbumInterface[]}
+ */
+export const searchAlbumByName = (
+  albums: AlbumInterface[],
+  text: string
+): AlbumInterface[] => {
   if (!text) {
     return albums;
   }
@@ -13,7 +29,12 @@ export const searchAlbumByName = (albums: AlbumInterface[], text: string) => {
   return albums.filter(({ name }) => name.toLowerCase().includes(searchText));
 };
 
-export const shortAlbums = (albums: AlbumInterface[]) => {
+/**
+ * Function that short albums by name
+ * @param albums {AlbumInterface[]}
+ * @returns {AlbumInterface[]}
+ */
+export const shortAlbums = (albums: AlbumInterface[]): AlbumInterface[] => {
   return [...albums].sort((a: AlbumInterface, b: AlbumInterface) => {
     const nameA = a.name.toUpperCase();
     const nameB = b.name.toUpperCase();
